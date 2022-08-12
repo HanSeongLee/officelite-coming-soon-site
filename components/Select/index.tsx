65210af418c7e9968dddc20c226b88d264eaf82f
HanSeongLee <jkhanseong@gmail.com>
2022-08-12 23:02:51 +0900

    ADD | Select component

---
+++ components/Select/index.tsx
@@ -0,0 +1,59 @@
+import React, {HTMLAttributes, useCallback, useState} from 'react';
+import styles from './style.module.scss';
+import {Option} from '../../types/option';
+import cn from 'classnames';
+
+interface IProps extends HTMLAttributes<HTMLDivElement> {
+    name: string;
+    options: Option[];
+}
+
+const Select: React.FC<IProps> = ({ id, name, options, className }) => {
+    const [open, setOpen] = useState(false);
+    const [selected, setSelected] = useState(0);
+
+    const toggleOpen = useCallback(() => {
+        setOpen(!open);
+    }, [open]);
+
+    const onItemClick = useCallback((e, index) => {
+        e.stopPropagation();
+        setSelected(index);
+        setOpen(false);
+    }, []);
+
+    return (
+        <div className={cn(styles.select, {
+            [styles.open]: open,
+        }, className)}
+             onClick={toggleOpen}
+        >
+            <div className={styles.label}>
+                {options?.length > 0 ? options[selected].label : ''}
+            </div>
+
+            <div className={styles.listWrapper}>
+                <ul className={styles.list}>
+                    {options?.map((option, index) => (
+                        <li className={cn(styles.item, {
+                            [styles.checked]: selected === index,
+                        })}
+                            onClick={(e) => onItemClick(e, index)}
+                            key={index}
+                        >
+                            <div className={styles.label}>
+                                {option.label}
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <input type={'hidden'}
+                   name={name}
+                   value={options?.length > 0 ? options[selected].value : ''}
+            />
+        </div>
+    );
+};
+
+export default Select;
