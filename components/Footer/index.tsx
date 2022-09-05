1befd298089665e17dda4b33613b805379b6644e
HanSeongLee <jkhanseong@gmail.com>
2022-09-05 22:59:29 +0900

    ADD | Link component in Footer component

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import styles from './style.module.scss';
 import Container from '../Container';
 import Button from '../Button';
 import OpenCountdown from '../OpenCountdown';
+import {useRouter} from 'next/router';
 
 const Footer: React.FC = () => {
+    const route = useRouter();
+
+    const onClick = useCallback(() => {
+        route.push('/sign-up');
+    }, [route]);
+
     return (
         <footer className={styles.footer}>
             <Container className={styles.container}>
-                <OpenCountdown theme={'dark'} />
-                <Button className={styles.button}>
+                <OpenCountdown theme={'dark'}/>
+                <Button className={styles.button}
+                        onClick={onClick}
+                >
                     Get Started
                 </Button>
             </Container>
